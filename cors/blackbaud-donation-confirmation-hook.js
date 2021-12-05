b66b4e188a54f9cc4b470b2b19275e34de485130
Arif Bandali <hello@arifb.com>
2021-12-04 19:20:29 -0800

    cors poc

---
+++ cors/blackbaud-donation-confirmation-hook.js
@@ -0,0 +1,24 @@
+// Dependency on the axios library
+//ie. <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>;
+
+const subdomain = "medford";
+const response = postDonationToHyc();
+console.log(response);
+
+//Access-Control-Allow-Origin: http://
+
+async function postDonationToHyc() {
+  const response = await axios({
+    method: "post",
+    url: "http://hyc.devbox:3000/api/v1/donations",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: {
+      firstName: "Fred",
+      lastName: "Flintstone",
+      subdomain,
+    },
+  });
+  return response;
+}
